b7e5724e6ddcd69ddae8941ca6517b3949f19154
rakhi05 <rakhi662006@gmail.com>
2026-02-09 00:03:31 +0530

    evenodd

---
+++ Evenodd.java
@@ -0,0 +1,16 @@
+public class Evenodd {
+    public static void main(String[] args) {
+        int number = 10; // Example number to check
+
+        if (number % 2 == 0) {
+            System.out.println("even");
+        } 
+    else{   
+        System.out.println("odd");
+        }        
+
+    }
+}
+
+
+
