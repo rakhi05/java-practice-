eed4f1e20f4ff75d034870523e36547e55ba2e3e
rakhi05 <rakhi662006@gmail.com>
2026-02-25 23:10:37 +0530

    sqrt

---
+++ sqrt.java
@@ -0,0 +1,8 @@
+ public class sqrt{
+    public static void main(String[] args){
+        double number = 36.0;
+        double result = Math.sqrt(number);
+        System.out.println("square root is ;" + result);
+        
+    }
+ } 
