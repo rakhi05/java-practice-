30bbf7f30cc3d91276ee4dcc46266d1f23456bf5
rakhi05 <rakhi662006@gmail.com>
2026-03-14 20:54:44 +0530

    cheacknum

---
+++ Checknum.java
@@ -0,0 +1,14 @@
+class Checknum{
+    public static void main(String[] args){
+        int num=10;
+        if(num>0){
+            System.out.println("positive no.");
+        }
+        else if(num<0){
+            System.out.println("negative no.");
+        }
+        else{
+            System.out.println("zero");
+        }
+    }
+}
