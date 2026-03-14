f553657f18d7a31f8ddc5da6b9e84e9e7ff2e334
rakhi05 <rakhi662006@gmail.com>
2026-03-14 20:56:02 +0530

    switch

---
+++ Days.java
@@ -0,0 +1,22 @@
+ public class Days{
+    public static void main (String[] args){
+        int day= 5;
+        switch(day){
+        case 1:
+        System.out.println("monday");
+        break;
+        case 2:
+        System.out.println("tuesday");
+        break;
+        case 3:
+        System.out.println("wednesday");
+        break;
+        case 4:
+        System.out.println("thursday");
+        break;
+        default:
+        System.out.println("jine ke hai char din");
+
+        }
+    }
+}
