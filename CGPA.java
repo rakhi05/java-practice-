9aaaeb16f8d22dcde3e061334ace1272defb6e53
rakhi05 <rakhi662006@gmail.com>
2026-06-29 20:16:16 +0530

    find cgpa

---
+++ CGPA.java
@@ -0,0 +1,13 @@
+//write a program to calculate cgpa using marks of three subjects (out of 100) and display the result.
+
+public class CGPA {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 17;
+        int c = 6;
+        int sum = a+b+c;
+        float cgpa = sum/30.0f;
+        System.out.println(cgpa); 
+    }
+    
+}
