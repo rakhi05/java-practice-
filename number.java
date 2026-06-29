729b1120ecf1cb109124daa2deb91dd402ac872c
rakhi05 <rakhi662006@gmail.com>
2026-06-30 00:04:25 +0530

    datatype of no.

---
+++ number.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class number {
+    public static void main(String[] args) {
+        System.out.println("Enter a number");
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.hasNextInt());
+    }
+
+    
+}
